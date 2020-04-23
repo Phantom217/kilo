2c2ce6bab94e6a49e69d8fbeba1e6581a80865ed
Tyler Oalman <tyleroalman@gmail.com>
2020-04-22 22:12:36 -0500

    Clear lines as they are redrawn

--- src/kilo.c
+++ src/kilo.c
@@ -145,6 +145,7 @@ void editor_draw_rows(struct abuf *ab) {
     for (y = 0; y < E.screenrows; y++) {
         abAppend(ab, "~", 1);
 
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
         }
@@ -155,7 +156,6 @@ void editor_refresh_screen() {
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editor_draw_rows(&ab);
