ddcdd65ef68498cdbb52cdbb600ce8990b933ac9
Tyler Oalman <tyleroalman@gmail.com>
2020-04-22 22:09:18 -0500

    Hide the cursor when repainting

--- src/kilo.c
+++ src/kilo.c
@@ -154,12 +154,14 @@ void editor_draw_rows(struct abuf *ab) {
 void editor_refresh_screen() {
     struct abuf ab = ABUF_INIT;
 
+    abAppend(&ab, "\x1b[?25l", 6);
     abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editor_draw_rows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
