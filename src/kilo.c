d36e5d96877f6d9edd08cb8ab0e93f277317ea32
Tyler Oalman <tyleroalman@gmail.com>
2020-04-23 22:50:34 -0500

    Add support to display multiple lines

--- src/kilo.c
+++ src/kilo.c
@@ -44,7 +44,7 @@ struct editorConfig {
     int screenrows;
     int screencols;
     int numrows;
-    erow row;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -202,6 +202,19 @@ int get_window_size(int *rows, int *cols) {
     }
 }
 
+/*** row operations ***/
+
+void editor_append_row(char *s, size_t len) {
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+
 /*** file i/o ***/
 
 void editor_open(char *filename) {
@@ -213,18 +226,12 @@ void editor_open(char *filename) {
     char *line = NULL;
     size_t linecap = 0;
     ssize_t linelen;
-    linelen = getline(&line, &linecap, fp);
-    if (linelen != -1) {
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
         while (linelen > 0 &&
                (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) {
             linelen--;
         }
-
-        E.row.size = linelen;
-        E.row.chars = malloc(linelen + 1);
-        memcpy(E.row.chars, line, linelen);
-        E.row.chars[linelen] = '\0';
-        E.numrows = 1;
+        editor_append_row(line, linelen);
     }
     free(line);
     fclose(fp);
@@ -282,11 +289,11 @@ void editor_draw_rows(struct abuf *ab) {
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row.size;
+            int len = E.row[y].size;
             if (len > E.screencols) {
                 len = E.screencols;
             }
-            abAppend(ab, E.row.chars, len);
+            abAppend(ab, E.row[y].chars, len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -380,6 +387,7 @@ void init_editor() {
     E.cx = 0;
     E.cy = 0;
     E.numrows = 0;
+    E.row = NULL;
 
     if (get_window_size(&E.screenrows, &E.screencols) == -1) {
         die("get_window_size");
