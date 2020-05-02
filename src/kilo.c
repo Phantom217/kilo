1938c05766551222b10ed4b6818e57b34ac91d00
Tyler Oalman <tyleroalman@gmail.com>
2020-05-01 22:07:24 -0500

    Keep track of whether text loaded in editor was modified

--- src/kilo.c
+++ src/kilo.c
@@ -55,6 +55,7 @@ struct editorConfig {
     int screencols;
     int numrows;
     erow *row;
+    int dirty;
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
@@ -274,6 +275,7 @@ void editor_append_row(char *s, size_t len) {
     editor_update_row(&E.row[at]);
 
     E.numrows++;
+    E.dirty++;
 }
 
 void editor_row_insert_char(erow *row, int at, int c) {
@@ -285,6 +287,7 @@ void editor_row_insert_char(erow *row, int at, int c) {
     row->size++;
     row->chars[at] = c;
     editor_update_row(row);
+    E.dirty++;
 }
 
 /*** editor operations ***/
@@ -340,6 +343,7 @@ void editor_open(char *filename) {
     }
     free(line);
     fclose(fp);
+    E.dirty = 0;
 }
 
 void editor_save() {
@@ -356,6 +360,7 @@ void editor_save() {
             if (write(fd, buf, len) == len) {
                 close(fd);
                 free(buf);
+                E.dirty = 0;
                 editor_set_status_message("%d bytes written to disk", len);
                 return;
             }
@@ -458,8 +463,9 @@ void editor_draw_rows(struct abuf *ab) {
 void editor_draw_status_bar(struct abuf *ab) {
     abAppend(ab, "\x1b[7m", 4);
     char status[80], rstatus[80];
-    int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-                       E.filename ? E.filename : "[No Name]", E.numrows);
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
+                       E.filename ? E.filename : "[No Name]", E.numrows,
+                       E.dirty ? "(modified)" : "");
     int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
     if (len > E.screencols) {
         len = E.screencols;
@@ -638,6 +644,7 @@ void init_editor() {
     E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.dirty = 0;
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
