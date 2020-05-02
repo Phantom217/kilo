029a3c48bd599e8a4ca10b033a4d1a38a458f25f
Tyler Oalman <tyleroalman@gmail.com>
2020-05-01 22:11:47 -0500

    Confirm quit with unsaved changes

--- src/kilo.c
+++ src/kilo.c
@@ -21,6 +21,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k)&0x1f)
 
@@ -568,6 +569,8 @@ void editor_move_cursor(int key) {
 }
 
 void editor_process_keypress() {
+    static int quit_times = KILO_QUIT_TIMES;
+
     int c = editor_read_key();
 
     switch (c) {
@@ -576,6 +579,14 @@ void editor_process_keypress() {
             break;
 
         case CTRL_KEY('q'):
+            if (E.dirty && quit_times > 0) {
+                editor_set_status_message(
+                    "WARNING!!! File has unsaved changes. "
+                    "Press Ctrl-Q %d more times to quit.",
+                    quit_times);
+                quit_times--;
+                return;
+            }
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
@@ -632,6 +643,8 @@ void editor_process_keypress() {
             editor_insert_char(c);
             break;
     }
+
+    quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
